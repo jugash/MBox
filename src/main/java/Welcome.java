5fabc7f7e6d9a333e0977524a8f54af69245bc7a
jugash <jugash.ch@gmail.com>
2020-03-09 23:01:22 +0000

    ingress practice

---
+++ src/main/java/Welcome.java
@@ -0,0 +1,31 @@
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class Welcome extends AbstractHandler {
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello " + request.getPathInfo().replace("/", "") + "</h1>");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(8080);
+        server.setHandler(new Welcome());
+
+        server.start();
+        server.join();
+    }
+}
